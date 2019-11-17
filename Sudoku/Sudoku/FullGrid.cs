fddba1a975483a58ad383e911d63df2eaee8df7f
Olivier Roussel <rousselolivier@hotmail.com>
2019-11-17 08:51:06 +0100

    Sudoku (WIP)

---
+++ Sudoku/Sudoku/FullGrid.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class Cell : INotifyPropertyChanged
+    {
+        public Int16 Value { get; set; }
+
+        private bool _IsValidated;
+        public bool IsValidated {
+            get { return _IsValidated; }
+            set
+            {
+                _IsValidated = value;
+                OnPropertyChanged(nameof(IsValidated));
+            }
+}
+        public Cell()
+        {
+            Value = 0;
+            IsValidated = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class Row
+    {
+        public List<Cell> Cells;
+
+        public Row()
+        {
+            Cells = new List<Cell>();
+        }
+    }
+
+    public class Column
+    {
+        public List<Cell> Cells;
+
+        public Column()
+        {
+            Cells = new List<Cell>();
+        }
+    }
+
+    public class SubGrid
+    {
+        public BindingList<Cell> Cells { get; set; }
+
+        public SubGrid()
+        {
+            Cells = new BindingList<Cell>();
+        }
+    }
+
+    public class FullGrid
+    {
+        public BindingList<Cell> Cells { get; set; }
+        public BindingList<SubGrid> SubGrids { get; set; }
+        private List<Row> _rows;
+        private List<Column> _columns;
+
+        public FullGrid()
+        {
+            Cells = new BindingList<Cell>();
+            _rows = new List<Row>(9);
+            _columns = new List<Column>(9);
+            SubGrids = new BindingList<SubGrid>();
+            Enumerable.Range(1, 9).ToList().ForEach((i)=>_rows.Add(new Row()));
+            Enumerable.Range(1, 9).ToList().ForEach((i) => _columns.Add(new Column()));
+            Enumerable.Range(1, 9).ToList().ForEach((i) => SubGrids.Add(new SubGrid()));
+            
+            foreach (Row row in _rows)
+            {
+                foreach (Column col in _columns)
+                {
+                    var cell = new Cell();
+                    Cells.Add(cell);
+                    row.Cells.Add(cell);
+                    col.Cells.Add(cell);
+                }
+            }
+
+            for (int rowId = 0; rowId < 9; rowId++)
+            {
+                for (int colId = 0; colId < 9; colId++)
+                {
+                    int subGridId = 3*(rowId % 3) + (colId % 3);
+                    SubGrids[subGridId].Cells.Add(_rows[rowId].Cells[colId]);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Sudoku/Sudoku/SudokuVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sudoku
+{
+    public class SudokuVM
+    {
+        public FullGrid MainGrid { get; set; }
+        public ICommand ValidateCellsCmd { get; set; }
+
+        public SudokuVM()
+        {
+            MainGrid = new FullGrid();
+            ValidateCellsCmd = new RelayCommand<object>((o) => ValidateCells());
+        }
+
+        private void ValidateCells()
+        {
+            foreach (Cell cell in MainGrid.Cells)
+            {
+                if (cell.Value > 0)
+                    cell.IsValidated = true;
+            }
+        }
+
+
+    }
+}
