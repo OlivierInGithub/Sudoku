86e95100dd964485c80999c41934d71dadd497ef
Olivier Roussel <rousselolivier@hotmail.com>
2019-11-18 22:42:42 +0100

    Sudoku with buttons

---
+++ Sudoku/Sudoku/CellValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Sudoku
+{
+    class CellValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var toReturn = value.ToString();
+            if (toReturn == "0")
+            {
+                toReturn = " ";
+            }
+            return toReturn;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Sudoku/Sudoku/SudokuVM.cs
+++ Sudoku/Sudoku/SudokuVM.cs
@@ -1,32 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows.Input;
 
 namespace Sudoku
 {
     public class SudokuVM
     {
-        public FullGrid MainGrid { get; set; }
+        private FullGrid _mainGrid { get; set; }
+        public BindingList<SubGridVM> SubGrids { get; set; }
         public ICommand ValidateCellsCmd { get; set; }
 
         public SudokuVM()
         {
-            MainGrid = new FullGrid();
+            _mainGrid = new FullGrid();
+            SubGrids = new BindingList<SubGridVM>();
+            foreach (SubGrid subGrid in _mainGrid.SubGrids)
+            {
+                SubGrids.Add(new SubGridVM(subGrid));
+            }
             ValidateCellsCmd = new RelayCommand<object>((o) => ValidateCells());
         }
 
         private void ValidateCells()
         {
-            foreach (Cell cell in MainGrid.Cells)
+            foreach (SubGridVM subGrid in SubGrids)
             {
-                if (cell.Value > 0)
-                    cell.IsValidated = true;
+                foreach (CellVM cell in subGrid.Cells)
+                {
+                    if (cell.Value > 0)
+                        cell.IsValidated = true;
+                }
+            }
+        }
+    }
+
+    public class SubGridVM
+    {
+        private SubGrid _subGrid;
+        public BindingList<CellVM> Cells { get; set; }
+
+        public SubGridVM(SubGrid subGrid)
+        {
+            _subGrid = subGrid;
+            Cells = new BindingList<CellVM>();
+            foreach (Cell cell in _subGrid.Cells)
+            {
+                Cells.Add(new CellVM(cell));
+            }
+        }
+    }
+
+    public class CellVM: INotifyPropertyChanged
+    {
+        private Cell _cell;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public short Value
+        {
+            get
+            {
+                return _cell.Value;
+            }
+            set
+            {
+                _cell.Value = value;
+                OnPropertyChanged(nameof(Value));
             }
         }
 
+        public bool IsValidated
+        {
+            get
+            {
+                return _cell.IsValidated;
+            }
+            set
+            {
+                _cell.IsValidated = true;
+                OnPropertyChanged(nameof(IsValidated));
+            }
+        }
 
+        public ICommand CellClickCmd { get; set; }
+
+        public CellVM(Cell cell)
+        {
+            _cell = cell;
+            CellClickCmd = new RelayCommand<object>((o) => CellClick());
+        }
+
+        private void CellClick()
+        {
+            var newValue = Value + 1;
+            if (newValue > 9)
+                newValue = 0;
+            Value = (short)newValue;
+        }
     }
 }
